b62d95af642a8fa25990b0a286feee67fb7c1c28
Vlad <webz@bk.ru>
2020-10-18 22:10:50 +0300

    fix CW-Master.py

--- CW-Master.py
+++ CW-Master.py
@@ -1,4 +1,10 @@
 import wx
+import data
+import play
+
+
+def audio(message, wpm=35, fs=33, sps=48000, freq=900):
+    play.main(message, wpm, fs, sps, freq)
 
 class MyFrame(wx.Frame):
     count = 0
@@ -31,13 +37,24 @@ class MyFrame(wx.Frame):
         bStart.Bind(wx.EVT_BUTTON, self.onStart)
         bStop.Bind(wx.EVT_BUTTON, self.onStop)
 
+
     def onStart(self, e):
         self.count = self.count + 1
         self.gauge.SetValue(self.count)
+        wpm = 35
+        call_sent = data.get_callsign(1)[0]
+        serial_sent = data.get_serial(3)
+        msg = call_sent + ' 5NN ' + serial_sent[1]
+        audio(msg, wpm)
+        #     call_recd, serial_recd = input().split()
+        #     data.check_callsign(call_sent, call_recd.upper())
+        #     data.check_serial(serial_sent[0], serial_recd)
         if self.count >= 50:
             print('>50')
     def onStop(self, e):
         pass
+
+
 
 app = wx.App()
 frame = MyFrame(None, 'wxPython')
